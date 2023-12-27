ab5e7ee65b2a4629477d56f0ba7ab911004f120a
Mongketolga <mongketolga@gmail.com>
2023-12-27 21:40:38 +0800

    customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { CustomersTableType } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { Suspense } from 'react';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return <CustomersTable customers={customers} />;
 }
