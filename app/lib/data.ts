9fdd16c2c3163f235372a4f5a0195780c5fc04e8
Mongketolga <mongketolga@gmail.com>
2023-12-28 12:39:10 +0800

    handling errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -25,7 +25,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -168,6 +168,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
